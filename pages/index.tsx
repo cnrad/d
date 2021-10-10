937308a7e5c596ac3f8034c15980948ccdff29d1
cnrad <83192247+cnrad@users.noreply.github.com>
2021-10-10 18:24:23 -0400

    really basic design proto

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,31 @@ export const getStaticProps = async function () {
 const Home: NextPage = ({ twitter }: any) => {
     return (
         <Page>
-            <SectionBox>Twitter Follower Count: {twitter.followers}</SectionBox>
+            <SectionBox>
+                <SectionTitle>TWITTER</SectionTitle>
+                <SectionInfo>
+                    Followers:
+                    <SectionStat color={"#2462AF"}>{twitter.followers}</SectionStat>
+                </SectionInfo>
+            </SectionBox>
+
+            <SectionBox>
+                <SectionTitle>GITHUB</SectionTitle>
+                <SectionInfo>
+                    Followers:
+                    <SectionStat color={"#2462AF"}>{twitter.followers}</SectionStat>
+                </SectionInfo>
+
+                <SectionInfo>
+                    Views:
+                    <SectionStat color={"#2462AF"}>{twitter.followers}</SectionStat>
+                </SectionInfo>
+
+                <SectionInfo>
+                    Stars:
+                    <SectionStat color={"#2462AF"}>{twitter.followers}</SectionStat>
+                </SectionInfo>
+            </SectionBox>
         </Page>
     );
 };
@@ -30,12 +54,41 @@ const Page = styled.div`
 `;
 
 const SectionBox = styled.div`
+    font-family: Open Sans;
     width: 20rem;
     height: 15rem;
     background: #181c2f;
     color: #fff;
     border-radius: 0.35rem;
     filter: drop-shadow(2px 2px 5px rgba(0, 0, 0, 0.1));
+    padding: 1.5rem;
+    margin: 1rem;
+`;
+
+const SectionTitle = styled.h1`
+    letter-spacing: 0.05rem;
+    color: #8a8d96;
+    height: auto;
+    width: 100%;
+    font-size: 0.85rem;
+    margin: 0 0 1rem 0;
+`;
+
+const SectionInfo = styled.div`
+    color: #fff;
+    height: auto;
+    width: 100%;
+    font-size: 1.25rem;
+    margin: 0;
+    display: flex;
+    flex-direction: row;
+`;
+
+const SectionStat = styled.div<{ color: string }>`
+    color: ${({ color }) => color};
+    font-size: 1.25rem;
+    margin-left: 0.5rem;
+    font-weight: 500;
 `;
 
 export default Home;
